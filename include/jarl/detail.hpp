186c78d9e8696baaeb915e1e7b483cba328f1a81
mikhailshevtsov <mikhail.shevtsoff@gmail.com>
2025-06-23 20:37:05 +0000

    Recovery commit

--- include/jarl/detail.hpp
+++ include/jarl/detail.hpp
@@ -2,35 +2,54 @@
 #define JARL_DETAIL_HPP
 
 #include <cstddef>
-#include <string_view>
 
-#define JARL_SIZE_TYPE jarl::size_type
-#define JARL_NAME_TYPE jarl::name_type
-#define JARL_THIS_TYPE this_type
-#define JARL_DEFAULT_ENUM_TYPE void
+#define JARL_TAG jarl::tag<this_type>
+#define JARL_INDEX(INDEX) std::integral_constant<jarl::size_type, INDEX>
+#define JARL_DEFINE_CURRENT_INDEX(NAME) NAME ##_index(JARL_TAG);
+#define JARL_CURRENT_INDEX(NAME) decltype(NAME ##_index(JARL_TAG{}))
+#define JARL_DECLARE_NEXT_INDEX(NAME) JARL_INDEX(JARL_CURRENT_INDEX(NAME){} + 1)
+#define JARL_STATIC_STRING(STRING) []() { using namespace jarl::detail; return #STRING ##_s; }()
+//#define JARL_STATIC_STRING(STRING) []<std::size_t... Is>(std::index_sequence<Is...>) -> jarl::detail::static_string<#STRING[Is]...> { return {}; } (std::make_index_sequence<std::size(#STRING) - 1>{})
 
-#define JARL_INDEX(INDEX) std::integral_constant<JARL_SIZE_TYPE, INDEX>
-#define JARL_ADL jarl::detail::adl<JARL_THIS_TYPE>
-#define JARL_DEFINE_CURRENT_INDEX(NAME) NAME ##_index(JARL_ADL);
-#define JARL_CURRENT_INDEX(NAME) decltype(NAME ##_index(JARL_ADL{}))
-#define JARL_NEXT_INDEX(NAME) JARL_INDEX(JARL_CURRENT_INDEX(NAME){} + 1)
-
+/// @brief Identity macro that expands to its arguments unchanged.
+/// 
+/// Can be used to force macro expansion or to improve readability.
+/// @param ... Any arguments.
+/// @return The same arguments passed in.
 #define JARL_TYPE(...) __VA_ARGS__
+
+/// @brief Alias for `JARL_TYPE` macro.
+/// @param ... Any arguments.
+/// @return The same arguments passed in.
 #define JT JARL_TYPE
 
 namespace jarl
 {
 
-using size_type = std::size_t;
-using name_type = std::string_view;
-
 namespace detail
 {
 
-template <typename>
-struct adl {};
+template <typename CharT, CharT... Cs>
+struct static_string
+{
+    template <CharT... OtherCs>
+    constexpr static_string<CharT, Cs..., OtherCs...> operator+(static_string<CharT, OtherCs...>) const noexcept { return {}; }
+
+    constexpr operator const char*() const noexcept { return data; }
+
+    static constexpr const char data[] {Cs..., '\0'};
+};
+
+template <typename CharT, CharT... Cs>
+constexpr static_string<CharT, Cs...> operator""_s() noexcept { return {}; }
 
 }
+
+/// @brief Alias for the unsigned integer type used for sizes and counts..
+using size_type = std::size_t;
+
+template <typename>
+struct tag {};
 
 }
 
--- include/jarl/struct.hpp
+++ include/jarl/struct.hpp
@@ -10,20 +10,22 @@
 #define JARL_FIELD(TYPE, NAME, ...) \
 JARL_DEFINE_CURRENT_INDEX(NAME) \
 TYPE NAME{ __VA_OPT__(__VA_ARGS__) }; \
-friend constexpr JARL_NAME_TYPE get_type_hint(JARL_ADL, JARL_CURRENT_INDEX(NAME)) noexcept { return #TYPE; } \
-friend constexpr JARL_NAME_TYPE get_name(JARL_ADL, JARL_CURRENT_INDEX(NAME)) noexcept { return #NAME; } \
-friend constexpr const auto& get_field(const JARL_THIS_TYPE& s, JARL_CURRENT_INDEX(NAME)) noexcept { return s.NAME; } \
-friend constexpr auto& get_field(JARL_THIS_TYPE& s, JARL_CURRENT_INDEX(NAME)) noexcept { return s.NAME; } \
-friend constexpr TYPE get_type(JARL_ADL, JARL_CURRENT_INDEX(NAME)); \
-friend constexpr JARL_NEXT_INDEX(NAME)
+friend constexpr auto get_type_hint(JARL_TAG, JARL_CURRENT_INDEX(NAME)) noexcept { return #TYPE; } \
+friend constexpr auto get_name(JARL_TAG, JARL_CURRENT_INDEX(NAME)) noexcept { return #NAME; } \
+friend constexpr const auto& get_field(const this_type& s, JARL_CURRENT_INDEX(NAME)) noexcept { return s.NAME; } \
+friend constexpr auto& get_field(this_type& s, JARL_CURRENT_INDEX(NAME)) noexcept { return s.NAME; } \
+friend constexpr TYPE get_type(JARL_TAG, JARL_CURRENT_INDEX(NAME)); \
+friend constexpr auto get_full_name(JARL_TAG, JARL_CURRENT_INDEX(NAME)) noexcept { return get_static_name(JARL_TAG{}) + JARL_STATIC_STRING(::NAME); } \
+friend constexpr JARL_DECLARE_NEXT_INDEX(NAME)
 
 #define JARL_DEFINE_STRUCT(STRUCT_NAME, ...) \
-using JARL_THIS_TYPE = STRUCT_NAME; \
-friend constexpr void is_struct(JARL_ADL) noexcept {} \
-friend constexpr JARL_NAME_TYPE get_name(JARL_ADL) noexcept { return #STRUCT_NAME; } \
+using this_type = STRUCT_NAME; \
+friend constexpr void is_struct(JARL_TAG) noexcept {} \
+friend constexpr auto get_name(JARL_TAG) noexcept { return #STRUCT_NAME; } \
+friend constexpr auto get_static_name(JARL_TAG) noexcept { return JARL_STATIC_STRING(STRUCT_NAME); } \
 friend constexpr JARL_INDEX(0) \
 __VA_ARGS__ \
-get_size(JARL_ADL) noexcept { return {}; } \
+get_size(JARL_TAG) noexcept { return {}; } \
 
 #define JARL_STRUCT(STRUCT_NAME, STRUCT_BODY) \
 struct STRUCT_NAME \
@@ -39,37 +41,38 @@ namespace jarl
 namespace detail
 {
 
-template <typename S, size_type... Is>
+template <typename StructT, size_type... Is>
 class struct_traits
 {
 public:
-    using struct_type = S;
+    using struct_type = StructT;
 
     template <size_type I>
-    using field_type = std::tuple_element<I, std::tuple<decltype(get_type(adl<S>{}, JARL_INDEX(Is){}))...>>;
+    using field_type = std::tuple_element<I, std::tuple<decltype(get_type(tag<StructT>{}, JARL_INDEX(Is){}))...>>;
 
-    static constexpr name_type name() noexcept { return get_name(adl<S>{}); }
+    static constexpr auto name() noexcept { return get_name(tag<StructT>{}); }
     static constexpr const auto& type_hints() noexcept { return _type_hints; }
     static constexpr const auto& names() noexcept { return _names; }
-    static constexpr size_type size() noexcept { return get_size(adl<S>{}); }
+    static constexpr const auto& full_names() noexcept { return _full_names; }
+    static constexpr auto size() noexcept { return get_size(tag<StructT>{}); }
 
-    static constexpr auto make_tuple(const S& s) noexcept { return std::make_tuple(std::cref(get_field(s, JARL_INDEX(Is){}))...); }
-    static constexpr auto make_tuple(S& s) noexcept { return std::make_tuple(std::ref(get_field(s, JARL_INDEX(Is){}))...); }
-    static constexpr auto make_tuple(S&& s) noexcept { return std::make_tuple(std::move(get_field(s, JARL_INDEX(Is){}))...); }
+    static constexpr auto make_tuple(const StructT& s) noexcept { return std::make_tuple(std::cref(get_field(s, JARL_INDEX(Is){}))...); }
+    static constexpr auto make_tuple(StructT& s) noexcept { return std::make_tuple(std::ref(get_field(s, JARL_INDEX(Is){}))...); }
+    static constexpr auto make_tuple(StructT&& s) noexcept { return std::make_tuple(std::move(get_field(s, JARL_INDEX(Is){}))...); }
 
-    static constexpr auto get(const S& s, size_type index)
+    static constexpr auto get(const StructT& s, size_type index)
     {
         assert_index(index);
         return _const_getters[index](s);
     }
 
-    static constexpr auto get(S& s, size_type index)
+    static constexpr auto get(StructT& s, size_type index)
     {
         assert_index(index);
         return _getters[index](s);
     }
 
-    static constexpr auto get(S&& s, size_type index)
+    static constexpr auto get(StructT&& s, size_type index)
     {
         assert_index(index);
         return _move_getters[index](s);
@@ -86,67 +89,71 @@ private:
     template <typename T>
     using array_t = std::array<const T, sizeof...(Is)>;
 
-    static constexpr array_t<name_type> _type_hints = { get_type_hint(adl<S>{}, JARL_INDEX(Is){})... };
-    static constexpr array_t<name_type> _names = { get_name(adl<S>{}, JARL_INDEX(Is){})... };
+    static constexpr array_t<const char*> _type_hints = { get_type_hint(tag<StructT>{}, JARL_INDEX(Is){})... };
+    static constexpr array_t<const char*> _names = { get_name(tag<StructT>{}, JARL_INDEX(Is){})... };
+    static constexpr array_t<const char*> _full_names = { get_full_name(tag<StructT>{}, JARL_INDEX(Is){})... };
 
-    using const_variant_t = std::variant<std::reference_wrapper<const decltype(get_type(adl<S>{}, JARL_INDEX(Is){}))>...>;
-    using variant_t = std::variant<std::reference_wrapper<decltype(get_type(adl<S>{}, JARL_INDEX(Is){}))>...>;
-    using move_variant_t = std::variant<decltype(get_type(adl<S>{}, JARL_INDEX(Is){}))...>;
+    using const_variant_t = std::variant<std::reference_wrapper<const decltype(get_type(tag<StructT>{}, JARL_INDEX(Is){}))>...>;
+    using variant_t = std::variant<std::reference_wrapper<decltype(get_type(tag<StructT>{}, JARL_INDEX(Is){}))>...>;
+    using move_variant_t = std::variant<decltype(get_type(tag<StructT>{}, JARL_INDEX(Is){}))...>;
 
-    static constexpr array_t<const_variant_t(*)(const S&)> _const_getters = { [](const S& s) -> const_variant_t { return std::get<Is>(make_tuple(s)); }... };
-    static constexpr array_t<variant_t(*)(S&)> _getters = { [](S& s) -> variant_t { return std::get<Is>(make_tuple(s)); }... };
-    static constexpr array_t<move_variant_t(*)(S&&)> _move_getters = { [](S&& s) -> move_variant_t { return std::get<Is>(make_tuple(std::move(s))); }... };
+    static constexpr array_t<const_variant_t(*)(const StructT&)> _const_getters = { [](const StructT& s) -> const_variant_t { return std::get<Is>(make_tuple(s)); }... };
+    static constexpr array_t<variant_t(*)(StructT&)> _getters = { [](StructT& s) -> variant_t { return std::get<Is>(make_tuple(s)); }... };
+    static constexpr array_t<move_variant_t(*)(StructT&&)> _move_getters = { [](StructT&& s) -> move_variant_t { return std::get<Is>(make_tuple(std::move(s))); }... };
 };
 
-template <typename S, typename>
+template <typename StructT, typename>
 struct make_struct_traits{};
 
-template <typename S, size_type... Is>
-struct make_struct_traits<S, std::index_sequence<Is...>> { using type = struct_traits<S, Is...>; };
+template <typename StructT, size_type... Is>
+struct make_struct_traits<StructT, std::index_sequence<Is...>> { using type = struct_traits<StructT, Is...>; };
 
-template <typename S, std::size_t N>
-using make_struct_traits_t = typename make_struct_traits<S, decltype(std::make_index_sequence<N>{})>::type;
+template <typename StructT, std::size_t N>
+using make_struct_traits_t = typename make_struct_traits<StructT, decltype(std::make_index_sequence<N>{})>::type;
 
 }
 
 template <typename T>
-concept meta_struct = requires{ is_struct(jarl::detail::adl<std::decay_t<T>>{}); };
+concept meta_struct = requires{ is_struct(jarl::tag<std::decay_t<T>>{}); };
 
-template <meta_struct S>
-using struct_traits = detail::make_struct_traits_t<std::decay_t<S>, get_size(detail::adl<std::decay_t<S>>{})>;
+template <meta_struct StructT>
+using struct_traits = detail::make_struct_traits_t<std::decay_t<StructT>, get_size(tag<std::decay_t<StructT>>{})>;
 
-template <size_type I, meta_struct S>
-using field_type_t = typename struct_traits<S>::template field_type<I>;
+template <size_type I, meta_struct StructT>
+using field_type_t = typename struct_traits<StructT>::template field_type<I>;
 
-template <meta_struct S>
-constexpr name_type struct_name() noexcept { return struct_traits<S>::name(); }
+template <meta_struct StructT>
+constexpr auto struct_name() noexcept { return struct_traits<StructT>::name(); }
 
-template <meta_struct S>
-constexpr const auto& struct_type_hints() noexcept { return struct_traits<S>::type_hints(); }
+template <meta_struct StructT>
+constexpr const auto& struct_type_hints() noexcept { return struct_traits<StructT>::type_hints(); }
 
-template <meta_struct S>
-constexpr const auto& struct_names() noexcept { return struct_traits<S>::names(); }
+template <meta_struct StructT>
+constexpr const auto& struct_names() noexcept { return struct_traits<StructT>::names(); }
 
-template <meta_struct S>
-constexpr size_type struct_size() noexcept { return struct_traits<S>::size(); }
+template <meta_struct StructT>
+constexpr const auto& struct_full_names() noexcept { return struct_traits<StructT>::full_names(); }
 
-template <meta_struct S>
-constexpr auto make_tuple(S&& s) { return struct_traits<S>::make_tuple(std::forward<S>(s)); }
+template <meta_struct StructT>
+constexpr auto struct_size() noexcept { return struct_traits<StructT>::size(); }
 
-template <size_type I, meta_struct S>
-constexpr decltype(auto) get(S&& s) noexcept { return std::get<I>(make_tuple(std::forward<S>(s))); }
+template <meta_struct StructT>
+constexpr auto make_tuple(StructT&& s) { return struct_traits<StructT>::make_tuple(std::forward<StructT>(s)); }
 
-template <meta_struct S>
-constexpr auto get(S&& s, size_type index) { return struct_traits<S>::get(std::forward<S>(s), index); }
+template <size_type I, meta_struct StructT>
+constexpr decltype(auto) get(StructT&& s) noexcept { return std::get<I>(make_tuple(std::forward<StructT>(s))); }
 
-template <meta_struct S>
-constexpr auto get(S&& s, name_type name)
+template <meta_struct StructT>
+constexpr auto get(StructT&& s, size_type index) { return struct_traits<StructT>::get(std::forward<StructT>(s), index); }
+
+template <meta_struct StructT>
+constexpr auto get(StructT&& s, std::string_view name)
 {
     size_type index = 0;
-    for (; index < struct_size<S>(); ++index)
-        if (name == struct_names<S>()[index])
+    for (; index < struct_size<StructT>(); ++index)
+        if (name == struct_names<StructT>()[index])
             break;
-    return get(std::forward<S>(s), index);
+    return get(std::forward<StructT>(s), index);
 }
 
 template <typename T, typename... Ts>
